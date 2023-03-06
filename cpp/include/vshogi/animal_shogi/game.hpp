899550be49d6a1af9b9a5a9b4f297ceb0370d60d
ctgk <r1135nj54w@gmail.com>
2023-03-06 22:00:04 +0900

    ADD: vshogi::animal_shogi::Game class

---
+++ cpp/include/vshogi/animal_shogi/game.hpp
@@ -0,0 +1,51 @@
+#ifndef VSHOGI_ANIMAL_SHOGI_GAME_HPP
+#define VSHOGI_ANIMAL_SHOGI_GAME_HPP
+
+#include <deque>
+#include <string>
+
+namespace vshogi::animal_shogi
+{
+
+/**
+ * @brief SFEN string representing default initial state of Animal Shogi.
+ *
+ */
+constexpr char default_initial_sfen[] = "";
+
+struct State
+{
+};
+struct Action
+{
+};
+
+/**
+ * @brief Pair of a state and action. The action turns the state to the next
+ * one.
+ *
+ */
+struct StateActionPair
+{
+    State state;
+    Action action;
+};
+
+/**
+ * @brief Animal Shogi game.
+ *
+ */
+class Game
+{
+private:
+    std::deque<StateActionPair> m_history;
+    State m_current_state;
+
+public:
+    Game();
+    Game(const std::string& sfen);
+};
+
+} // namespace vshogi::animal_shogi
+
+#endif // VSHOGI_ANIMAL_SHOGI_GAME_HPP
---
+++ cpp/src/vshogi/animal_shogi/game.cpp
@@ -0,0 +1,14 @@
+#include "vshogi/animal_shogi/game.hpp"
+
+namespace vshogi::animal_shogi
+{
+
+Game::Game() : m_history(), m_current_state()
+{
+}
+
+Game::Game(const std::string&) : m_history(), m_current_state()
+{
+}
+
+} // namespace vshogi::animal_shogi
