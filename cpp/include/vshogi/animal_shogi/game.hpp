fc7c08115c3e178ae44c938a20bebdaa6d66ba29
ctgk <r1135nj54w@gmail.com>
2023-03-08 13:04:33 +0900

    ADD: vshogi::animal_shogi::Game some public methods

--- cpp/include/vshogi/animal_shogi/game.hpp
+++ cpp/include/vshogi/animal_shogi/game.hpp
@@ -1,6 +1,7 @@
 #ifndef VSHOGI_ANIMAL_SHOGI_GAME_HPP
 #define VSHOGI_ANIMAL_SHOGI_GAME_HPP
 
+#include <cstdint>
 #include <deque>
 #include <string>
 
@@ -17,8 +18,13 @@ namespace vshogi::animal_shogi
  */
 struct StateActionPair
 {
-    State state;
+    std::uint64_t hashed_state;
     Move action;
+
+    StateActionPair(const std::uint64_t s, const Move a)
+        : hashed_state(s), action(a)
+    {
+    }
 };
 
 /**
@@ -34,6 +40,33 @@ private:
 public:
     Game();
     Game(const std::string& sfen);
+
+    ColorEnum get_turn() const
+    {
+        return m_current_state.get_turn();
+    }
+    const Board& get_board() const
+    {
+        return m_current_state.get_board();
+    }
+    const PieceStand& get_piece_stand(const ColorEnum c) const
+    {
+        return m_current_state.get_piece_stand(c);
+    }
+    bool is_move_legal(const Move move) const
+    {
+        return m_current_state.is_move_legal(move);
+    }
+    Game& apply_move(const Move move)
+    {
+        m_history.emplace_back(StateActionPair(m_current_state.hash(), move));
+        m_current_state.apply_move(move);
+        return *this;
+    }
+    std::size_t nth_move() const
+    {
+        return m_history.size() + 1;
+    }
 };
 
 } // namespace vshogi::animal_shogi
