b30c06f34fc07ee218368a455c66c0caa7d7f24f
ctgk <r1135nj54w@gmail.com>
2023-03-12 03:16:24 +0000

    ADD: README.md

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ _vshogi_extension = Pybind11Extension(
 setup(
     ext_modules=[_vshogi_extension],
     cmdclass={'build_ext': build_ext},
-    description='Shogi API for Shogi and its variants',
     classifiers=[
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: GNU General Public License (GPL)',
