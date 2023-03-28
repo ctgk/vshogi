FIX: use softmax function to prevent overflow

python/vshogi/mcts/_mcts.py
@@ -2,6 +2,7 @@ import typing as tp
 from copy import deepcopy
 
 import numpy as np
+import scipy.special as sp
 
 
 Policy = tp.Dict['Move', float]
@@ -211,9 +212,8 @@ class MonteCarloTreeSearcher:
         """
         d = getattr(self, f'get_{by}')()
         moves = list(d.keys())
-        v = list(d.values())
+        v = np.array(list(d.values()))
         if temperature is None:
             return moves[np.argmax(v)]
-        probas = np.array(v) ** (1 / temperature)
-        probas = probas / np.sum(probas)
+        probas = sp.softmax(np.log(v + 1e-3) / temperature)
         return np.random.choice(moves, p=probas)