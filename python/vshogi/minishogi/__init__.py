f48d6824f0ef46ae48e39756588c85713035887a
ctgk <r1135nj54w@gmail.com>
2023-05-06 04:46:08 +0000

    ADD: example usage of minishogi module

--- python/vshogi/minishogi/__init__.py
+++ python/vshogi/minishogi/__init__.py
@@ -1,6 +1,53 @@
 """Minishogi module.
 
 cf. https://en.wikipedia.org/wiki/Minishogi
+
+Examples
+--------
+>>> from vshogi.minishogi import *
+>>> game = Game()
+>>> print(game)
+Turn: BLACK
+White: -
+    5   4   3   2   1
+  *---*---*---*---*---*
+A |-HI|-KA|-GI|-KI|-OU|
+  *---*---*---*---*---*
+B |   |   |   |   |-FU|
+  *---*---*---*---*---*
+C |   |   |   |   |   |
+  *---*---*---*---*---*
+D |+FU|   |   |   |   |
+  *---*---*---*---*---*
+E |+OU|+KI|+GI|+KA|+HI|
+  *---*---*---*---*---*
+Black: -
+>>> game.apply(Move(SQ_4C, SQ_2E))
+Game(sfen="rbsgk/4p/1B3/P4/KGS1R w - 2")
+>>> game.apply(Move(SQ_1C, SQ_1B))
+Game(sfen="rbsgk/5/1B2p/P4/KGS1R b - 3")
+>>> game.apply(Move(SQ_2E, SQ_1E))
+Game(sfen="rbsgk/5/1B2p/P4/KGSR1 w - 4")
+>>> game.apply(Move(SQ_1D, SQ_1C))
+Game(sfen="rbsgk/5/1B3/P3p/KGSR1 b - 5")
+>>> game.apply(Move(SQ_2A, SQ_2E, promote=True))
+Game(sfen="rbs+Rk/5/1B3/P3p/KGS2 w G 6")
+>>> print(game)
+BLACK_WIN
+White: -
+    5   4   3   2   1
+  *---*---*---*---*---*
+A |-HI|-KA|-GI|+RY|-OU|
+  *---*---*---*---*---*
+B |   |   |   |   |   |
+  *---*---*---*---*---*
+C |   |+KA|   |   |   |
+  *---*---*---*---*---*
+D |+FU|   |   |   |-FU|
+  *---*---*---*---*---*
+E |+OU|+KI|+GI|   |   |
+  *---*---*---*---*---*
+Black: KI
 """
 
 import numpy as np
