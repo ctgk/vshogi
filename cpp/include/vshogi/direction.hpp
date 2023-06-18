e09d509d65e7854854b1834a4cb2012ae91cc60c
ctgk <r1135nj54w@gmail.com>
2023-06-18 22:46:19 +0900

    ADD: vshogi::shogi::SquareEnum

--- cpp/include/vshogi/direction.hpp
+++ cpp/include/vshogi/direction.hpp
@@ -7,9 +7,11 @@ namespace vshogi
 enum DirectionEnum
 {
     // clang-format off
-    DIR_NW = 0,  DIR_N = 1, DIR_NE = 2,
-     DIR_W = 3,              DIR_E = 4,
-    DIR_SW = 5,  DIR_S = 6, DIR_SE = 7,
+    DIR_NNW = 8,             DIR_NNE = 9,
+     DIR_NW = 0,  DIR_N = 1,  DIR_NE = 2,
+      DIR_W = 3,               DIR_E = 4,
+     DIR_SW = 5,  DIR_S = 6,  DIR_SE = 7,
+    DIR_SSW =10,             DIR_SSE = 11,
     // clang-format on
 };
 
