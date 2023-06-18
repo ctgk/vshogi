bdb40844f75cab45d508c1b57a7e92079572993b
ctgk <r1135nj54w@gmail.com>
2023-06-18 10:20:55 +0900

    DELETE: vshogi.Direction

--- cpp/python/vshogi.cpp
+++ cpp/python/vshogi.cpp
@@ -1,5 +1,4 @@
 #include "vshogi/color.hpp"
-#include "vshogi/direction.hpp"
 #include "vshogi/result.hpp"
 
 #include <pybind11/numpy.h>
@@ -17,18 +16,6 @@ void export_color_enum(py::module& m)
         .value("BLACK", vshogi::BLACK)
         .value("WHITE", vshogi::WHITE);
 }
-void export_direction_enum(py::module& m)
-{
-    py::enum_<vshogi::DirectionEnum>(m, "Direction")
-        .value("NORTHWEST", vshogi::DIR_NW)
-        .value("NORTH", vshogi::DIR_N)
-        .value("NORTHEAST", vshogi::DIR_NE)
-        .value("WEST", vshogi::DIR_W)
-        .value("EAST", vshogi::DIR_E)
-        .value("SOUTHWEST", vshogi::DIR_SW)
-        .value("SOUTH", vshogi::DIR_S)
-        .value("SOUTHEAST", vshogi::DIR_SE);
-}
 void export_result_enum(py::module& m)
 {
     py::enum_<vshogi::ResultEnum>(m, "Result")
@@ -41,7 +28,6 @@ void export_result_enum(py::module& m)
 PYBIND11_MODULE(_vshogi, m)
 {
     export_color_enum(m);
-    export_direction_enum(m);
     export_result_enum(m);
 
     auto animal_shogi_module = m.def_submodule("animal_shogi");
