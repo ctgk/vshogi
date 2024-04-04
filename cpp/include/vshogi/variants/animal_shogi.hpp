a2da30b3e8303d94d7df87ff794812bc15d072dd
ctgk <r1135nj54w@gmail.com>
2024-04-04 18:04:45 +0900

    REMOVE: remove specialized definition `vshogi::animal_shogi::Game::to_feature_map()`

--- cpp/include/vshogi/variants/animal_shogi.hpp
+++ cpp/include/vshogi/variants/animal_shogi.hpp
@@ -644,48 +644,6 @@ animal_shogi::Game::apply_mcts_internal_vertex(const animal_shogi::Move& move)
 }
 
 template <>
-inline void animal_shogi::Game::to_feature_map(float* const data) const
-{
-    constexpr int num_squares = ranks() * files();
-    constexpr int sp_types = stand_piece_types();
-    constexpr int bp_types = board_piece_types();
-    constexpr int ch_half = sp_types + bp_types;
-    constexpr int ch = feature_channels();
-
-    const auto turn = get_turn();
-    const auto stand_curr = get_stand(turn);
-    const auto stand_next = get_stand(~turn);
-    const auto board = get_board();
-
-    for (int k = sp_types; k--;) {
-        const auto p = animal_shogi::Pieces::stand_piece_array[k];
-        const auto num_curr = static_cast<float>(stand_curr.count(p));
-        const auto num_next = static_cast<float>(stand_next.count(p));
-        for (int i = num_squares; i--;) {
-            data[i * ch + k] = num_curr;
-            data[i * ch + k + ch_half] = num_next;
-        }
-    }
-    for (int i = num_squares; i--;) {
-        const auto sq = static_cast<typename animal_shogi::Squares::SquareEnum>(
-            (turn == BLACK) ? i : (num_squares - 1 - i));
-        const auto board_piece = board[sq];
-        for (int k = bp_types; k--;) {
-            data[i * ch + k + sp_types] = 0.f;
-            data[i * ch + k + sp_types + ch_half] = 0.f;
-        }
-        if (board.is_empty(sq))
-            continue;
-        const auto color = animal_shogi::Pieces::get_color(board_piece);
-        const auto piece_type
-            = animal_shogi::Pieces::to_piece_type(board_piece);
-        auto k = static_cast<int>(piece_type);
-        k += (turn == color) ? 0 : ch_half;
-        data[i * ch + k + sp_types] = 1.f;
-    }
-}
-
-template <>
 inline animal_shogi::Game::Game(const animal_shogi::State& s)
     : m_current_state(s), m_zobrist_hash_list(), m_move_list(), m_legal_moves(),
       m_result(ONGOING), m_zobrist_hash(m_current_state.zobrist_hash()),
