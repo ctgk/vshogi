d4cedbec27249eb456b4fe1f14cd4f8fb6685003
ctgk <r1135nj54w@gmail.com>
2024-11-20 22:23:51 +0900

    ADD: DFPN tests on minishogi and judkins shogi

---
+++ python/tests/test_vshogi/test_minishogi/test_dfpn.py
@@ -0,0 +1,39 @@
+import pytest
+
+import vshogi.judkins_shogi as shogi
+from vshogi.engine import DfpnSearcher
+
+
+def test_mate():
+    sfen_searches_pair_list = [
+        ('4+P/1gk2/1p3/3+R1/5 b G2s2br', 4),
+    ]
+
+    actual_total_searches = 0
+    expect_total_searches = 0
+    searcher = DfpnSearcher()
+    for sfen, searches in sfen_searches_pair_list:
+        game = shogi.Game(sfen)
+        searcher.set_game(game)
+        searcher.search(n=100000)
+        if not searcher.found_mate():
+            print(game)
+            print(searcher._tree(depth=5, breadth=5))
+            assert False
+        if searcher.get_search_count() != searches:
+            print(
+                sfen,
+                f'actual={searcher.get_search_count()}',
+                f'expect={searches}',
+            )
+        actual_total_searches += searcher.get_search_count()
+        expect_total_searches += searches
+    print(
+        f'#searches_total: actual={actual_total_searches}, '
+        f'expected={expect_total_searches}',
+    )
+    assert actual_total_searches == expect_total_searches
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
