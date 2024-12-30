1b634182b82fb8e6e0cb352c859b1468e712c5a6
ctgk <r1135nj54w@gmail.com>
2024-12-30 11:50:22 +0900

    EDIT: refactor `Move`

--- cpp/include/vshogi/common/move.hpp
+++ cpp/include/vshogi/common/move.hpp
@@ -26,39 +26,29 @@ class Move
 {
 private:
     using C = Configuration<Parameters>;
-    static constexpr auto num_squares = C::num_squares;
-    static constexpr auto num_stand_piece_types = C::num_stand_piece_types;
-    static constexpr auto num_dir_dl = C::num_dir_dl;
-    using SquaresHelper = Squares<Parameters>;
-    using PiecesHelper = Pieces<Parameters>;
+    using SHelper = Squares<Parameters>;
+    using PHelper = Pieces<Parameters>;
     using Square = typename C::Square;
     using PieceType = typename C::PieceType;
 
-    using Int = std::uint16_t;
-    static constexpr uint msb_destination = 6u;
-    static constexpr uint msb_promotion = 7u;
-    static constexpr uint msb_source = 14u;
-    static constexpr uint source_shift = msb_promotion + 1u;
-    static constexpr uint promotion_shift = msb_destination + 1u;
-    static constexpr Int destination_mask
-        = static_cast<Int>((1U << (msb_destination + 1U)) - 1U);
-    static constexpr Int promotion_mask = static_cast<Int>(
-        (1U << (msb_promotion + 1U)) - 1U - destination_mask);
-    static constexpr Int source_mask = static_cast<Int>(
-        (1U << (msb_source + 1U)) - 1U - destination_mask - promotion_mask);
+    static constexpr uint source_shift = 8u;
+    static constexpr uint promotion_shift = 7u;
+    static constexpr std::uint16_t dst_mask = 0x007fu;
+    static constexpr std::uint16_t prm_mask = 0x0080u;
+    static constexpr std::uint16_t src_mask = 0x7f00u;
+    static constexpr std::uint16_t full_mask = 0x7fffu;
 
 private:
     /**
-     * @brief N-bit integer representing a move in a game.
+     * @brief 16-bit integer representing a move in a game.
     */
-    Int m_value;
+    std::uint16_t m_value;
 
 public:
     Move() : m_value()
     {
     }
-    Move(const Int value)
-        : m_value(value & (source_mask | promotion_mask | destination_mask))
+    Move(const std::uint16_t value) : m_value(value & full_mask)
     {
     }
     Move(
@@ -67,20 +57,19 @@ public:
     {
     }
     Move(const Square dst, const PieceType src) noexcept
-        : Move(dst, static_cast<uint>(src) + num_squares)
+        : Move(dst, static_cast<uint>(src) + C::num_squares)
     {
     }
     explicit Move(const char usi[5])
         : Move(
-            SquaresHelper::to_square(usi + 2),
-            (usi[1] == '*')
-                ? static_cast<uint>(PiecesHelper::to_piece_type(usi[0]))
-                      + num_squares
-                : static_cast<uint>(SquaresHelper::to_square(usi)),
+            SHelper::to_square(usi + 2),
+            (usi[1] == '*') ? static_cast<uint>(PHelper::to_piece_type(usi[0]))
+                                  + C::num_squares
+                            : static_cast<uint>(SHelper::to_square(usi)),
             usi[4] == '+')
     {
     }
-    Int hash() const
+    std::uint16_t hash() const
     {
         return m_value;
     }
@@ -88,12 +77,12 @@ public:
     {
         if (is_drop()) {
             usi[0] = static_cast<char>(
-                std::toupper(PiecesHelper::to_char(source_piece())));
+                std::toupper(PHelper::to_char(source_piece())));
             usi[1] = '*';
         } else {
-            SquaresHelper::to_usi(usi, source_square());
+            SHelper::to_usi(usi, source_square());
         }
-        SquaresHelper::to_usi(usi + 2, destination());
+        SHelper::to_usi(usi + 2, destination());
         usi[4] = (promote()) ? '+' : '\0';
     }
     bool operator==(const Move& other) const
@@ -106,7 +95,7 @@ public:
     }
     Square destination() const
     {
-        return static_cast<Square>(m_value & destination_mask);
+        return static_cast<Square>(m_value & dst_mask);
     }
     Square source_square() const
     {
@@ -115,16 +104,15 @@ public:
     PieceType source_piece() const
     {
         return static_cast<PieceType>(
-            static_cast<int>(m_value >> source_shift)
-            - static_cast<int>(num_squares));
+            static_cast<uint>(m_value >> source_shift) - C::num_squares);
     }
     bool promote() const
     {
-        return static_cast<bool>(m_value & promotion_mask);
+        return static_cast<bool>(m_value & prm_mask);
     }
     bool is_drop() const
     {
-        return static_cast<uint>(m_value >> source_shift) >= num_squares;
+        return static_cast<uint>(m_value >> source_shift) >= C::num_squares;
     }
     Move rotate() const
     {
@@ -136,10 +124,10 @@ public:
     }
     Move hflip() const
     {
-        const auto dst_hflipped = SquaresHelper::hflip(destination());
+        const auto dst_hflipped = SHelper::hflip(destination());
         if (is_drop())
             return Move(dst_hflipped, source_piece());
-        const auto src_hflipped = SquaresHelper::hflip(source_square());
+        const auto src_hflipped = SHelper::hflip(source_square());
         return Move(dst_hflipped, src_hflipped, promote());
     }
     uint to_dlshogi_policy_index() const
@@ -150,28 +138,27 @@ public:
     }
     static constexpr uint num_policy_per_square()
     {
-        return 2 * num_dir_dl + num_stand_piece_types;
+        return 2 * C::num_dir_dl + C::num_stand_piece_types;
     }
 
 private:
     Move(const Square dst, const uint src, const bool promote = false)
-        : m_value(static_cast<Int>(
+        : m_value(static_cast<std::uint16_t>(
             (src << source_shift)
             | static_cast<uint>(promote << promotion_shift) | dst))
     {
     }
     static constexpr Square rotate_square(const Square sq)
     {
-        return static_cast<Square>(
-            static_cast<int>(num_squares) - 1 - static_cast<int>(sq));
+        return static_cast<Square>(C::num_squares - 1u - static_cast<uint>(sq));
     }
     uint to_dlshogi_source_index() const
     {
         if (is_drop())
-            return num_dir_dl * 2 + static_cast<uint>(source_piece());
-        const uint promo_offset = promote() ? num_dir_dl : 0U;
+            return C::num_dir_dl * 2 + static_cast<uint>(source_piece());
+        const uint promo_offset = promote() ? C::num_dir_dl : 0U;
         const auto direction
-            = SquaresHelper::get_direction(source_square(), destination());
+            = SHelper::get_direction(source_square(), destination());
         return static_cast<uint>(direction) + promo_offset;
     }
 };
