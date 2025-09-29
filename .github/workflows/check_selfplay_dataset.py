89a6ef8f644e82a82329a26b20286e7563ddd2dd
ctgk <r1135nj54w@gmail.com>
2025-09-29 18:16:34 +0900

    ADD: script to check validity of selfplay generated dataset

---
+++ .github/workflows/check_selfplay_dataset.py
@@ -0,0 +1,56 @@
+import typing as tp
+
+from classopt import classopt, config
+import numpy as np
+import pandas as pd
+
+
+@classopt(default_long=True)
+class Args:
+    kifu: str = config(long=False)
+
+
+if __name__ == '__main__':
+    args = Args.from_args()
+    df = pd.read_csv(
+        args.kifu,
+        sep='\t',
+        usecols=['state', 'result', 'q_value', 'visit_count', 'z_weight'],
+        dtype={
+            'state': str,
+            'result': str,
+            'q_value': float,
+            'visit_count': str,
+            'z_weight': float,
+        },
+    )
+    if len(df) == 0:
+        raise ValueError(f"Empty kifu, {args.kifu}")
+
+    for i in range(len(df)):
+        row = df.iloc[i]
+        visit_count: dict = eval(row.visit_count)
+        if visit_count != {}:
+            continue
+
+        state = row.state
+        turn: tp.Union[tp.Literal['b'], tp.Literal['w']] = state.split(' ')[1]
+        if not np.isclose(abs(row.q_value), 1):
+            raise ValueError(
+                f"'{state}': `q_value`(={row.q_value}) should be 1 or -1 "
+                "when `visit_count` is empty."
+            )
+        for t, r in zip(('b', 'w'), ('BLACK_WIN', 'WHITE_WIN')):
+            if (turn == t) and (r in row.result):
+                if not np.isclose(row.q_value, 1):
+                    raise ValueError(
+                        f"'{state}': `q_value`(={row.q_value}) should be 1 "
+                        f"when the result is {r}"
+                    )
+        for t, r in zip(('w', 'b'), ('BLACK_WIN', 'WHITE_WIN')):
+            if (turn == t) and (r in row.result):
+                if not np.isclose(row.q_value, -1):
+                    raise ValueError(
+                        f"'{state}': `q_value`(={row.q_value}) should be -1 "
+                        f"when the result is {r}"
+                    )
