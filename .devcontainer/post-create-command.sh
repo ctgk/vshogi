0bad0a98d2fe390faf4b7571fec7ab6c141fa9e4
ctgk <r1135nj54w@gmail.com>
2025-11-24 17:32:12 +0900

    REMOVE: devcontainer

--- .devcontainer/post-create-command.sh
+++
@@ -1,9 +0,0 @@
-#!//bin/bash
-
-sudo apt update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt install -y --no-install-recommends cmake cpputest clang-format-15 clang-tidy-17
-python -m pip install -upgrade pip
-pip install -e .[develop]
-pre-commit install
-pre-commit install -t pre-push
