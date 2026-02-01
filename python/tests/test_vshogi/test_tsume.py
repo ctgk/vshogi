aa872a354d37e19e77a3e4fe41c3a33685ffb88a
ctgk <r1135nj54w@gmail.com>
2026-02-01 19:13:23 +0900

    ADD: `exact` parameter in `solve_tsumeshogi()` function

--- python/tests/test_vshogi/test_tsume.py
+++ python/tests/test_vshogi/test_tsume.py
@@ -12,17 +12,13 @@ from vshogi import solve_tsumeshogi
         ("6gk1/7nb/5S3/6R2/9/9/9/9/9 b -", ["3d3a+, 2a3a, G*3b"]),
         ('8k/9/8+R/9/9/9/9/9/9 w Gr2g3s2l2n2p', ["1a2a, G*2b", "P*1b, G*2b"]),
         ("7bk/8p/9/9/9/9/B8/9/9 b N9p2l2n2s2g", ["9g8h, P*2b, N*2c"]),
-        (
-            "9/2k3p1R/1pp1B3p/9/9/9/9/9/9 b 2G6Pr2g3s2lnp",
-            ["1b3b+, 7b6a, 5c6b+", "1b3b+, 7b6c, G*6d", "1b3b+, 7b8a, G*8b"],
-        ),
         (  # simple case
             "6+B1k/7+b1/9/8P/9/9/9/9/9 b RN10p4l3n4s4gr",
-            ["R*2a, 2b2a, N*2c, 1a1b, 1d1c+"],
+            ["R*2a, 2b2a, N*2c, 1a1b, 1d1c+", "R*2a, 2b2a, N*2c, 1a1b, 3a1c"],
         ),
         (
             "9/6l2/6+P2/8n/5RBk1/8+p/6P2/9/9 b S9p2l2n2s2gbr",
-            ["S*3d, 2e1e, 3e1g, 1e2d, 3d2c+", "S*3d, 2e1e, 3e1g, P*2e, 4e2e"],
+            ["S*3d, 2e1e, 3e1g, 1e2d, 3d2c+"],
         ),
         (
             "6bkn/9/9/6Pr+B/9/9/9/9/9 b GSN2p2l2n2s2gr",
@@ -31,8 +27,12 @@ from vshogi import solve_tsumeshogi
                 'N*3c, 2a1b, G*2b, 3a2b, S*2a',
             ],
         ),
+        (
+            "4bk3/8+r/5G3/4B4/9/9/9/9/9 b RS9p2l2n2s2g",
+            ["S*3b, 1b3b, R*3a, 3b3a, 5d6c+"],
+        ),
         (  # mate involving ranging attack at 3rd move
-            "6n1+P/4s2r1/7k1/5+rpB1/7P1/9/9/9/9 b BGS9p2l2n2s2g",
+            "6n1+P/4s2r1/7k1/5+rpB1/7P1/2s6/9/9/9 b BGS9p2l2n2s2g",
             [
                 "S*1d, 2c1d, G*1e, 1d2c, B*1d",
                 "S*1d, 2c3b, B*5d, 4d5d, G*4b",
@@ -45,11 +45,8 @@ from vshogi import solve_tsumeshogi
             ["R*3f, 3e3f, 1h1g, 1f2g, G*2h", "R*3f, 3e3f, 1h1g, 1f1e, G*1f"],
         ),
         (
-            "1ns2+N3/1r2k4/2ppp4/9/9/9/9/1B7/9 b B2Gr2g3s2n4l15p",
-            [
-                "G*4b, 5b6a, 4a5a, 6a6b, 4b5b, 6b7b, B*6a",
-                "G*4b, 5b6a, 4a5a, 6a7b, B*6a, 7b6b, 4b5b",
-            ],
+            "7n1/5+R2l/7pk/6g1p/6p2/7S1/9/9/9 b BGP9p2l2n2s2gbr",
+            ['B*2b, 1c2d, P*2e, 3d2e, G*3d, 2d3d, 4b4d'],
         ),
     ],
 )
@@ -68,6 +65,49 @@ def test_solve_tsumeshogi(sfen, expect):
     g = g.rotate()
     actual = solve_tsumeshogi(g, max_ply=len(expect[0].split(", ")))
     assert set(expect) == set(
+        [", ".join(m.to_sfen() for m in a) for a in actual]
+    )
+
+
+@pytest.mark.parametrize(
+    ["sfen", "expect", "expect_exact"],
+    [
+        (
+            "9/2k3g1R/1pp1B3p/9/9/9/9/9/9 b r2g3s2lnp",
+            [
+                "1b3b+, 7b6a, 3b6b",
+                "1b3b+, 7b6a, 5c6b+",
+                "1b3b+, 7b6a, G*6b",
+                "1b3b+, 7b6c, G*6d",
+                "1b3b+, 7b8a, G*8b",
+                "1b3b+, 7b8a, G*9b",
+            ],
+            [
+                # this is not an exact mate, because of "1b3b+, 7b6a, 3b6b".
+                # "1b3b+, 7b6a, G*6b"
+                "1b3b+, 7b6c, G*6d",
+                "1b3b+, 7b8a, G*8b",
+                "1b3b+, 7b8a, G*9b",
+            ],
+        ),
+    ],
+)
+def test_solve_tsumeshogi_exact(sfen, expect, expect_exact):
+    g = Game(sfen)
+    print(g)
+    actual = solve_tsumeshogi(
+        g,
+        max_ply=len(expect[0].split(", ")),
+        exact=False,
+    )
+    assert set(expect) == set(
+        [", ".join(m.to_sfen() for m in a) for a in actual]
+    )
+    for mate in solve_tsumeshogi(g, max_ply=len(expect[0].split(", "))):
+        assert mate in actual
+
+    actual = solve_tsumeshogi(g, max_ply=len(expect[0].split(", ")))
+    assert set(expect_exact) == set(
         [", ".join(m.to_sfen() for m in a) for a in actual]
     )
 
--- python/vshogi/_tsume.py
+++ python/vshogi/_tsume.py
@@ -16,7 +16,12 @@ def _is_unnecessary_declined_promotion(
     return type(move)(move.to_sfen() + "+") in [t[0] for t in out]
 
 
-def _search_defence(game: Game, n_tezume: int) -> list[tuple[Move]]:
+def _search_defence(
+    game: Game,
+    n_tezume: int,
+    *,
+    search_all: bool,
+) -> list[tuple[Move]]:
     if n_tezume <= 0:
         return []
     if n_tezume % 2 == 1:
@@ -31,13 +36,20 @@ def _search_defence(game: Game, n_tezume: int) -> list[tuple[Move]]:
     for m in legal_moves:
         is_aigoma = game.is_aigoma(m)
         with game._apply_context(m):
-            if mates := _search_offence(game, n_tezume=n_tezume - 1):
+            if mates := _search_offence(
+                game,
+                n_tezume=n_tezume - 1,
+                search_all=search_all,
+            ):
                 out.extend([(m, *mate) for mate in mates])
             elif (
                 not is_aigoma
                 or not out
                 or not _search_offence(
-                    game, n_tezume=n_tezume + 1, dst=m.destination
+                    game,
+                    n_tezume=n_tezume + 1,
+                    dst=m.destination,
+                    search_all=False,
                 )
             ):
                 forced_mate = False
@@ -75,20 +87,35 @@ def _is_tsumi_with_futile_block(game: Game) -> bool:
         if not game.is_aigoma(m):
             return False
         with game._apply_context(m):
-            if not _search_offence(game, n_tezume=1, dst=m.destination):
+            if not _search_offence(
+                game,
+                n_tezume=1,
+                dst=m.destination,
+                search_all=False,
+            ):
                 is_tsumi = False
         if not is_tsumi:
             return False
     return is_tsumi
 
 
-def _search_offence(game: Game, n_tezume: int, dst=None) -> list[tuple[Move]]:
+def _search_offence(
+    game: Game,
+    n_tezume: int,
+    *,
+    search_all: bool,
+    dst=None,
+) -> list[tuple[Move]]:
     if n_tezume < 0:
         return []
     assert game.turn == Color.BLACK
     assert n_tezume % 2 == 1
     out: list[tuple[Move, ...]] = []
-    check_moves = sorted(game.get_check_moves(), key=lambda m: not m.promote)
+    check_moves = sorted(
+        game.get_check_moves(),
+        key=lambda m: -m.promote,
+        # key=lambda m: 2 * m.is_drop() - m.promote,
+    )
     if dst is not None:
         check_moves = [m for m in check_moves if m.destination == dst]
     for m in check_moves:
@@ -97,37 +124,102 @@ def _search_offence(game: Game, n_tezume: int, dst=None) -> list[tuple[Move]]:
         with game._apply_context(m, banish=dst is not None):
             if game.result == Result.BLACK_WIN:
                 out.append((m,))
-            elif mates := _search_defence(game, n_tezume=n_tezume - 1):
+            elif mates := _search_defence(
+                game,
+                n_tezume=n_tezume - 1,
+                search_all=search_all,
+            ):
                 out.extend([(m, *mate) for mate in mates])
-        if out:
+        if not search_all and out:
             break
     if out or n_tezume > 1:
         return out
     for m in check_moves:
+        if _is_unnecessary_declined_promotion(m, out):
+            continue
         with game._apply_context(m):
             if _is_tsumi_with_futile_block(game):
                 out.append((m,))
-        if out:
+        if not search_all and out:
             break
     return out
 
 
-def _solve_tsumeshogi(game: Game, n_tezume: int) -> list[tuple[Move]]:
+def _solve_tsumeshogi(
+    game: Game,
+    n_tezume: int,
+    *,
+    search_all: bool,
+) -> list[tuple[Move]]:
+    t = game.turn
     if n_tezume % 2 == 0:
-        if game.turn == Color.BLACK:
+        if t == Color.BLACK:
             game = game.rotate()
-            mates = _search_defence(game, n_tezume=n_tezume)
+        mates = _search_defence(
+            game,
+            n_tezume=n_tezume,
+            search_all=search_all,
+        )
+        if t == Color.BLACK:
             return [tuple(m.rotate() for m in mate) for mate in mates]
-        return _search_defence(game, n_tezume=n_tezume)
+        return mates
     else:
-        if game.turn == Color.WHITE:
+        if t == Color.WHITE:
             game = game.rotate()
-            mates = _search_offence(game, n_tezume=n_tezume)
+        mates = _search_offence(
+            game,
+            n_tezume=n_tezume,
+            search_all=search_all,
+        )
+        if t == Color.WHITE:
             return [tuple(m.rotate() for m in mate) for mate in mates]
-        return _search_offence(game, n_tezume=n_tezume)
+        return mates
 
 
-def solve_tsumeshogi(game: Game, max_ply: int = 5) -> list[tuple[Move]]:
+def _is_exact(game: Game) -> bool:
+    is_exact = (
+        sum(
+            game.stand(
+                Color.BLACK if game.turn == Color.WHITE else Color.WHITE
+            ).values()
+        )
+        == 0
+    )
+    return is_exact
+
+
+def _remove_redundant_mate(
+    game: Game,
+    mates: list[tuple[Move]],
+) -> list[tuple[Move]]:
+    if not mates:
+        return []
+
+    out: list[tuple[Move]] = []
+    found_redundant_mate: bool = False
+    for m0 in set(mate[0] for mate in mates):
+        with game._apply_context(m0):
+            if len(mates[0]) == 1:
+                if _is_exact(game):
+                    out.append((m0,))
+                else:
+                    found_redundant_mate = True
+            else:
+                if removed := _remove_redundant_mate(
+                    game, [mate[1:] for mate in mates if mate[0] == m0]
+                ):
+                    out.extend([(m0, *mate) for mate in removed])
+                else:
+                    found_redundant_mate = True
+    return [] if found_redundant_mate and len(mates[0]) % 2 == 1 else out
+
+
+def solve_tsumeshogi(
+    game: Game,
+    max_ply: int = 5,
+    *,
+    exact: bool = True,
+) -> list[tuple[Move]]:
     """Solve tsumeshogi puzzle.
 
     Parameters
@@ -136,6 +228,9 @@ def solve_tsumeshogi(game: Game, max_ply: int = 5) -> list[tuple[Move]]:
         Game position of the puzzle.
     max_ply : int
         Maximum number of ply. Note that it does not count futile blocks.
+    exact : bool, optional
+        Return exact mates only if true, otherwise return all mates that
+        include redundant pieces left in stand. By default True.
 
     Returns
     -------
@@ -146,5 +241,10 @@ def solve_tsumeshogi(game: Game, max_ply: int = 5) -> list[tuple[Move]]:
         msg = f"`max_ply` must be an instance of int, but was {type(max_ply)}"
         raise TypeError(msg)
     for n_tezume in range(max_ply % 2, max_ply + 1, 2):
-        if out := _solve_tsumeshogi(game, n_tezume=n_tezume):
-            return out
+        if out := _solve_tsumeshogi(
+            game,
+            n_tezume=n_tezume,
+            search_all=True,
+        ):
+            return _remove_redundant_mate(game, out) if exact else out
+    return []
