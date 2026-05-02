2e6de1f8d6bdf5489b4ff9640292be89befe44d9
ctgk <r1135nj54w@gmail.com>
2026-05-02 18:49:03 +0900

    ADD: ResultEnum in Rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,7 @@ use crate::variants::minishogi::PieceTypeEnum;
 
 pub mod color;
 pub mod magic;
+pub mod result;
 pub mod utils;
 pub mod variants;
 
---
+++ rust/src/result.rs
@@ -0,0 +1,45 @@
+use crate::color::ColorEnum;
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ResultEnum {
+    Ongoing,
+    Draw,
+    BlackWin,
+    WhiteWin,
+}
+
+impl ResultEnum {
+    pub fn winner(self) -> ColorEnum {
+        match self {
+            Self::BlackWin => ColorEnum::Black,
+            Self::WhiteWin => ColorEnum::White,
+            Self::Ongoing | Self::Draw => ColorEnum::None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_winner_for_ongoing() {
+        assert_eq!(ResultEnum::Ongoing.winner(), ColorEnum::None);
+    }
+
+    #[test]
+    fn test_winner_for_draw() {
+        assert_eq!(ResultEnum::Draw.winner(), ColorEnum::None);
+    }
+
+    #[test]
+    fn test_winner_for_black_win() {
+        assert_eq!(ResultEnum::BlackWin.winner(), ColorEnum::Black);
+    }
+
+    #[test]
+    fn test_winner_for_white_win() {
+        assert_eq!(ResultEnum::WhiteWin.winner(), ColorEnum::White);
+    }
+}
